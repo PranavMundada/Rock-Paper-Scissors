1f8d9bc1752e762e1b217653d9a3dad858495c6f
Pranav Mundada <pjmundada2005@gmail.com>
2024-12-13 12:34:35 +0530

    Added UI

--- javascript.js
+++ javascript.js
@@ -1,87 +1,88 @@
-function playround(scores){
-    let humanChoice,computerChoice;
-    let rand=Math.floor(Math.random()*11)%3;
-    humanChoice=prompt("Enter Your choice:").toUpperCase();
-    if(rand==0){
-        computerChoice="ROCK";
-        if(humanChoice=="ROCK")
-        {
-            console.log("Round Tied!");
+let scores = { humanScore: 0, computerScore: 0 };
+let current_result;
+let flag=0;
+
+function playround(scores, humanChoice) {
+    if(flag==0)return;
+    let rand = Math.floor(Math.random()*10) % 3;
+    if (rand == 0) {
+        computerChoice = "ROCK";
+        if (humanChoice == "ROCK") {
+            current_result = "Round Tied!";
         }
-        else if(humanChoice=="PAPER")
-        {
-            console.log("You Win! Paper beats Rock.");
-            scores.humanScore+=1;
+        else if (humanChoice == "PAPER") {
+            scores.humanScore += 1;
+            current_result = "You Win! Paper beats Rock.";
         }
-        else if(humanChoice=="SCISSOR")
-        {
-            console.log("You Lose! Rock beats Scissor.");
-            scores.computerScore+=1;
-        }
-        else
-        {
-            console.log("WRONG INPUT");
+        else if (humanChoice == "SCISSOR") {
+            scores.computerScore += 1;
+            current_result = "You Lose! Rock beats Scissor.";
         }
     }
-    else if(rand==1){
-        computerChoice="PAPER";
-        if(humanChoice=="ROCK")
-            {
-                console.log("You Lose! Paper beats Rock.");
-                scores.computerScore+=1;
-            }
-            else if(humanChoice=="PAPER")
-            {
-                console.log("Round Tied!");
-            }
-            else if(humanChoice=="SCISSOR")
-            {
-                console.log("You Win! Scissor beats Paper.");
-                scores.humanScore+=1;
-            }
-            else
-            {
-                console.log("WRONG INPUT");
-            }
+    else if (rand == 1) {
+        computerChoice = "PAPER";
+        if (humanChoice == "ROCK") {
+            scores.computerScore += 1;
+            current_result = "You Lose! Paper beats Rock.";
+        }
+        else if (humanChoice == "PAPER") {
+            current_result = "Round Tied!";
+        }
+        else if (humanChoice == "SCISSOR") {
+            scores.humanScore += 1;
+            current_result = "You Win! Scissor beats Paper.";
+        }
     }
-    else{
-        computerChoice="SCISSORS";
-        if(humanChoice=="ROCK")
-            {
-                console.log("You Win! Rock beats Scissor.");
-                scores.humanScore+=1;
-            }
-            else if(humanChoice=="PAPER")
-            {
-                console.log("You Lose!Scissors beats Paper.");
-                scores.computerScore+=1;
-            }
-            else if(humanChoice=="SCISSOR")
-            {
-                console.log("Round Tied!");
-            }
-            else
-            {
-                console.log("WRONG INPUT");
-            }
+    else {
+        computerChoice = "SCISSORS";
+        if (humanChoice == "ROCK") {
+            scores.humanScore += 1;
+            current_result = "You Win! Rock beats Scissor.";;
+        }
+        else if (humanChoice == "PAPER") {
+            scores.computerScore += 1;
+            current_result = "You Lose!Scissors beats Paper.";
+        }
+        else if (humanChoice == "SCISSOR") {
+            current_result = "Round Tied!";
+        }
+    }
+    document.getElementById('human_score').innerText = `Your Score: ${scores.humanScore}`;
+    document.getElementById('computer_score').innerText = `Computer Score: ${scores.computerScore}`;
+    document.getElementById('currentResult').innerText=`Round Result: ${current_result}`;
+    if(scores.humanScore===5){
+        document.getElementById('Result').innerText='YOU WON THE GAME';
+        flag=0;
+    }
+    if(scores.computerScore===5){
+        document.getElementById('Result').innerText='YOU LOST THE GAME';
+        flag=0;
     }
 }
 
-function playgame(){
-    let scores={humanScore:0,computerScore:0};
-    for(let i=0;i<5;i++){
-        playround(scores);
-    }
-    if(scores.humanScore>scores.computerScore){
-        console.log("YOU WON THE GAME!");
-    }
-    else if(scores.humanScore<scores.computerScore){
-        console.log("YOU LOST THE GAME!");
-    }
-    else
-    {
-        console.log("GAME TIED!");
-    }
-}
+const rock = document.querySelector("#rock");
+rock.addEventListener("click", () => {
+    playround(scores, "ROCK");
+});
 
-playgame();
+const paper = document.querySelector("#paper");
+paper.addEventListener("click", () => {
+    playround(scores, "PAPER");
+});
+
+const scissors = document.querySelector("#scissors");
+scissors.addEventListener("click", () => {
+    playround(scores, "SCISSOR");
+});
+
+const restart=document.querySelector('#restart');
+restart.addEventListener("click",()=>{
+    flag=1;
+    scores.humanScore=0;
+    scores.computerScore=0;
+    document.getElementById('human_score').innerText = `Your Score: ${scores.humanScore}`;
+    document.getElementById('computer_score').innerText = `Computer Score: ${scores.computerScore}`;
+    document.getElementById('currentResult').innerText=`Round Result:`;
+    document.getElementById('Result').innerText=' ';
+    document.getElementById('restart').innerText="Restart";
+});
